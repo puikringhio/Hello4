2f19f19c439ed4b2db63546fc59eaeda4eec3733
puikringhio <a.lacagnina@ringmaster.it>
2017-01-12 14:42:42 +0100

    Add files via upload

---
+++ module.js
@@ -0,0 +1,13 @@
+var app = angular.module('myApp', []);
+
+app.controller('myApp', function($scope, hexafy) {
+  $scope.hex = hexafy.myFunc(255555);
+});
+app.service('hexafy', function() {
+    this.myFunc = function (x) {
+        return x.toString(16);
+    }
+});
+
+
+
