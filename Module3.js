72a20a21787ffac05eae3414b3115dbc0d69b60e
puikringhio <a.lacagnina@ringmaster.it>
2017-01-12 16:34:54 +0100

    Delete Module3.js

--- Module3.js
+++
@@ -1,6 +0,0 @@
-var tat = angular.module('tat', []);
-tat.controller('bap_controller3', function($scope, $http) {
-  $http.get("http://www.gazzetta.it/").then(function (response) {
-      $scope.myWelcome = response.data;
-  });
-});	
