f7b316b8fc73e763092e9f0da79debd562576314
puikringhio <a.lacagnina@ringmaster.it>
2017-01-13 17:42:29 +0100

    Add files via upload
    Secondo capitolo fatto e commentato

--- assets/js/controller/controller.js
+++ assets/js/controller/controller.js
@@ -1,9 +1,49 @@
 app.controller('AppCtl', function($scope) {
+    
+    $scope.errorBox = true;
+    $scope.centerBox = false;
+    
+    $scope.nome = "John";
+    
+    
     $scope.clickHandler = function(){
         window.alert("clicked!");
     }
+    
+    
+    
+    
     $scope.hoverHandler = function(){
-        window.alert("hover!");
+        //window.alert("hover!");
+        $scope.isHidden = !$scope.isHidden
     }
+    
+    
+    $scope.contacts = [
+        {
+            name: 'jon doe',
+            phone: '131312123',
+            email: 'asd@asdasd.it'
+        },
+        {
+            name: 'jssaon desdds',
+            phone: '12121221',
+            email: 'ewq@ewq.it'
+        }
+    ];
+    
+    $scope.background = "red";
+    
+    $scope.styleDemo = function(){
+        if(!$scope.styler){
+            return;
+        }
+        
+        return{
+            background: 'red',
+            fontWeight: 'bold'
+        }
+    }
+    
 });
 
