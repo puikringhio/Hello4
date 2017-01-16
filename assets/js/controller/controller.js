d01d456b73083db9da12dea7379123d7f310c73a
puikringhio <a.lacagnina@ringmaster.it>
2017-01-16 14:40:28 +0100

    Add files via upload

--- assets/js/controller/controller.js
+++ assets/js/controller/controller.js
@@ -3,15 +3,13 @@ app.controller('AppCtl', function($scope) {
     $scope.errorBox = true;
     $scope.centerBox = false;
     
-    $scope.nome = "John";
+    $scope.Unnome = "John";
     
     
     $scope.clickHandler = function(){
         window.alert("clicked!");
     }
-    
-    
-    
+       
     
     $scope.hoverHandler = function(){
         //window.alert("hover!");
@@ -21,18 +19,31 @@ app.controller('AppCtl', function($scope) {
     
     $scope.contacts = [
         {
-            name: 'jon doe',
-            phone: '131312123',
+            name: 'jon',
+            phone: '1111111',
             email: 'asd@asdasd.it'
         },
         {
-            name: 'jssaon desdds',
-            phone: '12121221',
+            name: 'jssaon',
+            phone: '222222',
             email: 'ewq@ewq.it'
+        },
+        {
+            name: 'asd',
+            phone: '44444444',
+            email: 'uio@ewq.it'
+        },
+        {
+            name: 'qwe',
+            phone: '5555555',
+            email: 'uyt@ewq.it'
+        },
+        {
+            name: 'jdfg',
+            phone: '66666666',
+            email: 'hg@asdasd.it'
         }
     ];
-    
-    $scope.background = "red";
     
     $scope.styleDemo = function(){
         if(!$scope.styler){
